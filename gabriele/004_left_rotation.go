ede5ac09ced2699b95854fde61536fb9c16946d4
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2017-07-25 13:57:00 +0200

    004_left_rotation.go

---
+++ gabriele/004_left_rotation.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N, d int
+	fmt.Scanf("%d", &N)
+	fmt.Scanf("%d", &d)
+
+	arr := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Scanf("%d", &arr[i])
+	}
+
+	res := make([]int, N)
+	for i := 0; i < N; i++ {
+		res[(i-d+N)%N] = arr[i]
+	}
+
+	for i := 0; i < N; i++ {
+		fmt.Printf("%d ", res[i])
+	}
+}
