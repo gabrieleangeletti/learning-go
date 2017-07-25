7d1a0e6fbe9b3afc84df7b259b65273cef8ae52b
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2017-07-25 15:31:21 +0200

    005_sparse_arrays.go

---
+++ gabriele/005_sparse_arrays.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, q int
+	var s string
+	index := make(map[string]int)
+
+	fmt.Scanf("%d", &n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%s", &s)
+		v, ok := index[s]
+		if ok {
+			index[s] = v + 1
+		} else {
+			index[s] = 1
+		}
+	}
+
+	fmt.Scanf("%d", &q)
+	for i := 0; i < q; i++ {
+		fmt.Scanf("%s", &s)
+		fmt.Println(index[s])
+	}
+}
