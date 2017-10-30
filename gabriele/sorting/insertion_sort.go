631d40a6ce4e64e2bd8c9c5cd7975da0bc209852
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2017-10-30 22:50:34 +0000

    Capitalized exported functions

--- gabriele/sorting/insertion_sort.go
+++ gabriele/sorting/insertion_sort.go
@@ -1,44 +1,44 @@
-/*
- * Insertion Sort algorithm.
- *
- * Input: sequence of integers A.
- * Output: sorted sequence.
- *
- * At each step j (starting from 1), get the j-th element in A, A[j].
- * We assume that A[0: j - 1] is sorted, which is surely true when j = 1.
- * Go backwards from i = j - 1 to 0 to find the right place in which insert
- * A[j]. When the right place is found, insert A[j] and go to the next step.
- */
-
 package main
 
 import "fmt"
 
 func main() {
-  a := [6]int{5, 2, 4, 6, 1, 3}  // average case
-  b := [6]int{1, 2, 3, 4, 5, 6}  // best case
-  c := [6]int{6, 5, 4, 3, 2, 1}  // worst case
-  d := []int{}  // empty array
+	a := [6]int{5, 2, 4, 6, 1, 3} // average case
+	b := [6]int{1, 2, 3, 4, 5, 6} // best case
+	c := [6]int{6, 5, 4, 3, 2, 1} // worst case
+	d := []int{}                  // empty array
 
-  insertionSort(a[:])
-  insertionSort(b[:])
-  insertionSort(c[:])
-  insertionSort(d[:])
+	InsertionSort(a[:])
+	InsertionSort(b[:])
+	InsertionSort(c[:])
+	InsertionSort(d[:])
 
-  fmt.Println(a)
-  fmt.Println(b)
-  fmt.Println(c)
-  fmt.Println(d)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
 }
 
-func insertionSort(arr []int) {
-  for j := 1; j < len(arr); j++ {
-    key := arr[j]
+// InsertionSort ...
+//
+// Insertion Sort algorithm.
+//
+// Input: sequence of integers A.
+// Output: sorted sequence.
+//
+// At each step j (starting from 1), get the j-th element in A, A[j].
+// We assume that A[0: j - 1] is sorted, which is surely true when j = 1.
+// Go backwards from i = j - 1 to 0 to find the right place in which insert
+// A[j]. When the right place is found, insert A[j] and go to the next step.
+//
+func InsertionSort(arr []int) {
+	for j := 1; j < len(arr); j++ {
+		key := arr[j]
 
-    i := j - 1
-    for ; i >= 0 && arr[i] > key; i-- {
-      arr[i + 1] = arr[i]
-    }
-    arr[i + 1] = key
-  }
+		i := j - 1
+		for ; i >= 0 && arr[i] > key; i-- {
+			arr[i+1] = arr[i]
+		}
+		arr[i+1] = key
+	}
 }
--- gabriele/sorting/selection_sort.go
+++ gabriele/sorting/selection_sort.go
@@ -1,60 +1,59 @@
-/*
- * Selection sort algorithm.
- *
- * Input: sequence of integers A.
- * Output: sorted sequence.
- *
- * At time step 0, get the smallest element in A and swap it
- * with A[0]. At time step 1, get the smallest element in A[1:]
- * and swap it with A[1]. Stop at step n - 2, because in step n - 1,
- * A[n - 1:] is just one element, which is the largest one in the
- * array, and the latest.
- */
-
 package main
 
 import (
-  "fmt";
-  "math";
+	"fmt"
+	"math"
 )
 
 func main() {
-  a := [6]int{5, 2, 4, 6, 1, 3}  // average case
-  b := [6]int{1, 2, 3, 4, 5, 6}  // best case
-  c := [6]int{6, 5, 4, 3, 2, 1}  // worst case
-  d := []int{}  // empty array
+	a := [6]int{5, 2, 4, 6, 1, 3} // average case
+	b := [6]int{1, 2, 3, 4, 5, 6} // best case
+	c := [6]int{6, 5, 4, 3, 2, 1} // worst case
+	d := []int{}                  // empty array
 
-  selectionSort(a[:])
-  selectionSort(b[:])
-  selectionSort(c[:])
-  selectionSort(d[:])
+	SelectionSort(b[:])
+	SelectionSort(c[:])
+	SelectionSort(d[:])
+	SelectionSort(a[:])
 
-  fmt.Println(a)
-  fmt.Println(b)
-  fmt.Println(c)
-  fmt.Println(d)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
 }
 
-func selectionSort(arr []int) {
-  for i := 0; i < len(arr) - 1; i++ {
-    swap(arr, i, minValue(arr, i, len(arr)))
-  }
+// SelectionSort ...
+//
+// Selection sort algorithm.
+// Input: sequence of integers A.
+// Output: sorted sequence.
+//
+// At time step 0, get the smallest element in A and swap it
+// with A[0]. At time step 1, get the smallest element in A[1:]
+// and swap it with A[1]. Stop at step n - 2, because in step n - 1,
+// A[n - 1:] is just one element, which is the largest one in the
+// array, and the latest.
+//
+func SelectionSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		swap(arr, i, minValue(arr, i, len(arr)))
+	}
 }
 
-func minValue(arr []int, start int, end int) (int, int) {
-  minValue := math.MaxInt32
-  minIndex := -1
-  for i := start; i < end; i++ {
-    if arr[i] < minValue {
-      minValue = arr[i]
-      minIndex = i
-    }
-  }
-  return minIndex
+func minValue(arr []int, start int, end int) int {
+	minValue := math.MaxInt32
+	minIndex := -1
+	for i := start; i < end; i++ {
+		if arr[i] < minValue {
+			minValue = arr[i]
+			minIndex = i
+		}
+	}
+	return minIndex
 }
 
 func swap(arr []int, index1 int, index2 int) {
-  tmp := arr[index1]
-  arr[index1] = arr[index2]
-  arr[index2] = tmp
+	tmp := arr[index1]
+	arr[index1] = arr[index2]
+	arr[index2] = tmp
 }
